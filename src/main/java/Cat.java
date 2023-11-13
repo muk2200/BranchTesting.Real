d5843efee1afbd2b98cde344e02601eaa592174c
muk2200 <mukh0001@stud.kea.dk>
2023-11-13 09:24:35 +0100

    New Change - Boolean n eat feature

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -4,4 +4,9 @@ public class Cat extends Animal{
     public void makeSound() {
 
     }
+
+    @Override
+    public boolean eat(String foodType) {
+        return false;
+    }
 }
--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -5,4 +5,9 @@ public class Dog extends Animal {
     public void makeSound() {
 
     }
+
+    @Override
+    public boolean eat(String foodType) {
+        return false;
+    }
 }
